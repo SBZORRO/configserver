9d47a2dcb587e398eacca5a4bade9dd7a9511c36
sbzorro <sbzorro@gmail.com>
2022-03-16 23:56:05 +0800

    ch5

---
+++ src/test/java/com/github/sbzorro/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sbzorro.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
